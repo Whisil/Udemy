c214ac5b6c4f4dfb71aa210eeba55ad4c4a89d5b
BenQo <electomandarin@gmail.com>
2020-05-12 13:16:58 +0300

    dryge dz

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+"use strict";
 
-let personalMovieDB = {
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
-  privat: false
+  privat: false,
 };
 
-let lastelyWatched = prompt("Один из последних просмотренных фильмов?");
-let filmRating = prompt("На сколько оцените его?");
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[lastelyWatched] = filmRating;
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
+
+personalMovieDB.count < 10
+  ? console.log("You've watched a few movies")
+  : personalMovieDB.count > 10 && personalMovieDB.count < 30
+  ? console.log("You're a classic one")
+  : personalMovieDB.count > 30
+  ? console.log("You're an addict")
+  : console.log("error");
 
 console.log(personalMovieDB);
